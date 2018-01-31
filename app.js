6a997ad447ff7101e923e7ef13c1465bd60d7b4a
sgc88 <sehribancheng@yahoo.com>
2018-01-30 23:49:29 -0800

    first cell added using table/tr/td also added canvas element

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
 $("#submit").on("click", function makeGrid(){
+  var newWidth = $("#sizeWidth").val();
+  var newHeigth = $("#sizeHeight").val();
+  console.log("you picked the size")
+
+  $("#cell").width(newWidth);
+  $("#cell").height(newHeigth);
+
+
   console.log("welcome to art with pixels page!");
+
 });
 
 var canvas, ctx,
@@ -10,11 +19,12 @@ var canvas, ctx,
         color: '#000000',
         size: 10,
         click: false,
-    },
+    }
 
 
-$('#color-picker').on('input', function () {
+$('#color-picker').on("input", function(){
     brush.color = this.value;
+
 });
 
 
