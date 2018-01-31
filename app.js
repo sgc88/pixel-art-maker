7a84a841e55e96dab351911a85f0c4348c9696b3
sgc88 <sehribancheng@yahoo.com>
2018-01-30 22:11:09 -0800

    color-picker is ready

--- app.js
+++ app.js
@@ -3,7 +3,19 @@ $("#submit").on("click", function makeGrid(){
   console.log("welcome to art with pixels page!");
 });
 
+var canvas, ctx,
+    brush = {
+        x: 0,
+        y: 0,
+        color: '#000000',
+        size: 10,
+        click: false,
+    },
 
+
+$('#color-picker').on('input', function () {
+    brush.color = this.value;
+});
 
 
 
